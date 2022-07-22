e6e4d522ba6c8d3d444493e08a76574d653ee7ea
lj <donutsoft.ljh@gmail.com>
2022-07-22 19:06:28 +0900

    apply tailwind

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import '$styles/global.css';
+    //import '$styles/global.css';
+    import '../app.css';
     import { session } from '$app/stores';
-  </script>
+</script>
   
   <svelte:head>
     <title>SvelteKit Auth JWT</title>
@@ -26,3 +27,55 @@
 <slot />
 
 <footer>Copyright 2020 Ninja Smoothies</footer>
+
+<style>
+  body{
+    @apply mx-5 my-10 text-xl tracking-normal bg-white
+  }
+
+  h1, h2, h3, h4, ul, li, a, p, input, label, button, div, footer{
+    @apply m-0 p-0 font-body text-gray-600
+  }
+
+  ul {
+    @apply list-none
+  }
+
+  nav {
+    @apply flex justify-between items-end mb-32
+  }
+
+  nav ul {
+    @apply flex items-center
+  }
+
+  nav li {
+    @apply ml-5
+  }
+
+  nav li a {
+    @apply uppercase font-bold text-sm block px-2 py-4 tracking-wide
+  }
+
+  .btn {
+    @apply rounded-xl bg-amber-200
+  }
+
+  form h2{
+    @apply text-4xl font-black mb-10
+  }
+
+  form {
+    @apply w-80 mx-0 my-auto p-7 shadow-blue-500/50 rounded-lg bg-white
+  }
+
+  input {
+    @apply px-2 py-3 rounded border-solid border-2 border-slate-100 text-base w-full
+  }
+
+  label {
+    @apply block mt-5 mx-0 mb-2
+  }
+
+  
+</style>
