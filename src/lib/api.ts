9085edea0a1ff3d033e745c545fbaa701efb6745
Chris Engelhard <c.engelhard@digi4care.nl>
2022-08-08 10:43:50 +0200

    prettyfied

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,28 +1,25 @@
 type Send = Promise<{
-    error?: string
-    success?: string
-    data?: object
-  }>
+	error?: string;
+	success?: string;
+	data?: object;
+}>;
 
 export const send = async (form: HTMLFormElement): Send => {
-    
-    const response = await fetch(form.action,{
-        method: form.method,
-        body: new FormData(form),
-        headers: {accept: 'application/json'},
-    });
+	const response = await fetch(form.action, {
+		method: form.method,
+		body: new FormData(form),
+		headers: { accept: 'application/json' }
+	});
 
-    return await response.json();
-}
+	return await response.json();
+};
 
 export const sendJson = async (form: HTMLFormElement): Send => {
-    
-    const response = await fetch(form.action,{
-        method: form.method,
-        body: JSON.stringify(form),
-        headers: {accept: 'application/json'},
-    });
+	const response = await fetch(form.action, {
+		method: form.method,
+		body: JSON.stringify(form),
+		headers: { accept: 'application/json' }
+	});
 
-    return await response.json();
-}
-
+	return await response.json();
+};
