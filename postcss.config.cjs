a3f822107341373d54ae2e26668470ca5e080f42
Chris Engelhard <c.engelhard@digi4care.nl>
2022-08-09 00:01:31 +0200

    more tailwind suport

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,36 @@
-module.exports = {
+/* jshint esversion: 9 */
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
+const useNanoCSS = !dev
+	? {
+			cssnano: {}
+	  }
+	: {};
+
+const config = {
+	syntax: 'postcss-scss',
 	plugins: {
+		autoprefixer: {
+			cascade: true
+		},
+		'postcss-flexbugs-fixes': {},
+		'postcss-import': {},
+		'postcss-preset-env': {
+			autoprefixer: {
+				flexbox: 'no-2009'
+			},
+			stage: 3,
+			features: {
+				'nesting-rules': false,
+				'custom-properties': false
+			}
+		},
+		'postcss-nesting': {},
+		'tailwindcss/nesting': {},
 		tailwindcss: {},
-		autoprefixer: {}
+		...useNanoCSS
 	}
 };
+
+module.exports = config;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	mode: 'jit',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			fontFamily: {
