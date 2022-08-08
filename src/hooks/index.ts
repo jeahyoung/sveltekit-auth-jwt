9b808895b0582a59c846d0d1ad6ec5ea33a66ffb
Chris Engelhard <c.engelhard@digi4care.nl>
2022-08-08 10:44:59 +0200

    fixt jsonwebtoken import for build

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,60 +1,62 @@
-import { db } from "$lib/database";
-import type { GetSession, Handle } from "@sveltejs/kit";
+import { db } from '$lib/database';
+import type { GetSession, Handle } from '@sveltejs/kit';
 import * as cookie from 'cookie';
-import { verify } from "jsonwebtoken";
-import "dotenv/config";
-import { createRefreshToken, maxAge, createAccessToken } from "$lib/jwt";
+import pkg from 'jsonwebtoken';
+const { verify } = pkg;
+
+import 'dotenv/config';
+import { createRefreshToken, maxAge, createAccessToken } from '$lib/jwt';
 
 type JwtPayload = {
-    id: number
-}
+	id: number;
+};
 
-export const handle: Handle =async ({ event, resolve }) => {
-    /* const cookieHeader = event.request.headers.get('cookie');
+export const handle: Handle = async ({ event, resolve }) => {
+	/* const cookieHeader = event.request.headers.get('cookie');
     const cookies = cookie.parse(cookieHeader ?? ''); */
-    //const { token, refresh_token } = cookie.parse(event.request.headers.get('cookie') || '');
-    let setCookie: string = "";
-   
-    const { accessToken, refreshToken } = cookie.parse(event.request.headers.get('cookie') || '');
-    const jwt_secret = process.env.JWT_SECRET || '';
-    //console.log("token==>",token);
-    if(accessToken){
-        const user = verify(accessToken, jwt_secret) as JwtPayload;
-        if(refreshToken){
-            
-            console.log("==1==");
-             if (user){
-                 const session = await db.user.findUnique({
-                     where: { 
-                        idRefreshToken: {
-                            id: user.id, refreshToken: refreshToken
-                        }
-                      },
-                     select: { id:true, email:true }
-                 });
-     
-                 if(session){
-                    console.log("==1==session==>",session);
-                     event.locals.user = { id: session.id, email: session.email };
-                 }
-             }
-        } else {
-            console.log("==2==");
-            if(user){
-                const newRefreshToken = createRefreshToken();
-                const updateUser = await db.user.update({
-                    where: {
-                        id: user.id
-                    },
-                    data: {
-                        refreshToken: newRefreshToken
-                    },
-                });
+	//const { token, refresh_token } = cookie.parse(event.request.headers.get('cookie') || '');
+	let setCookie: string = '';
 
-                if(updateUser){
-                    event.locals.user = { id: updateUser.id, email: updateUser.email };
-                    // newRefreshToken 을 cookie에 넣어줘야함
-                    /* response.headers.set('Set-Cookie', cookie.serialize('refreshToken', newRefreshToken, {
+	const { accessToken, refreshToken } = cookie.parse(event.request.headers.get('cookie') || '');
+	const jwt_secret = process.env.JWT_SECRET || '';
+	//console.log("token==>",token);
+	if (accessToken) {
+		const user = verify(accessToken, jwt_secret) as JwtPayload;
+		if (refreshToken) {
+			console.log('==1==');
+			if (user) {
+				const session = await db.user.findUnique({
+					where: {
+						idRefreshToken: {
+							id: user.id,
+							refreshToken: refreshToken
+						}
+					},
+					select: { id: true, email: true }
+				});
+
+				if (session) {
+					console.log('==1==session==>', session);
+					event.locals.user = { id: session.id, email: session.email };
+				}
+			}
+		} else {
+			console.log('==2==');
+			if (user) {
+				const newRefreshToken = createRefreshToken();
+				const updateUser = await db.user.update({
+					where: {
+						id: user.id
+					},
+					data: {
+						refreshToken: newRefreshToken
+					}
+				});
+
+				if (updateUser) {
+					event.locals.user = { id: updateUser.id, email: updateUser.email };
+					// newRefreshToken 을 cookie에 넣어줘야함
+					/* response.headers.set('Set-Cookie', cookie.serialize('refreshToken', newRefreshToken, {
                         // send cookie for every page
                         path: '/',
                         // server side only cookie so you can't use `document.cookie`
@@ -66,38 +68,34 @@ export const handle: Handle =async ({ event, resolve }) => {
                         // set cookie to expire after a month
                         maxAge: maxAge * 14
                     })); */
-                    setCookie = cookie.serialize('refreshToken', newRefreshToken, {
-                        // send cookie for every page
-                        path: '/',
-                        // server side only cookie so you can't use `document.cookie`
-                        httpOnly: true,
-                        // only requests from same site can send cookies and serves to protect from CSRF
-                        sameSite: 'strict',
-                        // only sent over HTTPS
-                        secure: process.env.NODE_ENV === 'production',
-                        // set cookie to expire after a month
-                        maxAge: maxAge * 14
-                    });
-                }
+					setCookie = cookie.serialize('refreshToken', newRefreshToken, {
+						// send cookie for every page
+						path: '/',
+						// server side only cookie so you can't use `document.cookie`
+						httpOnly: true,
+						// only requests from same site can send cookies and serves to protect from CSRF
+						sameSite: 'strict',
+						// only sent over HTTPS
+						secure: process.env.NODE_ENV === 'production',
+						// set cookie to expire after a month
+						maxAge: maxAge * 14
+					});
+				}
+			}
+		}
+	} else {
+		if (refreshToken) {
+			console.log('==3==');
+			const session = await db.user.findUnique({
+				where: { refreshToken: refreshToken },
+				select: { id: true, email: true }
+			});
 
-            }
-            
-        }
-        
-        
-    } else {
-        if(refreshToken){
-            console.log("==3==");
-            const session = await db.user.findUnique({
-                where: { refreshToken: refreshToken },
-                select: { id:true, email:true }
-            });
-
-            if(session){
-                const newAccessToken = createAccessToken(session.id, session.email);
-                event.locals.user = { id: session.id, email: session.email };
-                // newAccessToken 을 cookie에 넣어줘야함
-                /* response.headers.set('Set-Cookie', cookie.serialize('accessToken', newAccessToken, {
+			if (session) {
+				const newAccessToken = createAccessToken(session.id, session.email);
+				event.locals.user = { id: session.id, email: session.email };
+				// newAccessToken 을 cookie에 넣어줘야함
+				/* response.headers.set('Set-Cookie', cookie.serialize('accessToken', newAccessToken, {
                     // send cookie for every page
                     path: '/',
                     // server side only cookie so you can't use `document.cookie`
@@ -109,25 +107,23 @@ export const handle: Handle =async ({ event, resolve }) => {
                     // set cookie to expire after a month
                     maxAge: maxAge * 14
                 })); */
-                setCookie = cookie.serialize('accessToken', newAccessToken, {
-                    // send cookie for every page
-                    path: '/',
-                    // server side only cookie so you can't use `document.cookie`
-                    httpOnly: true,
-                    // only requests from same site can send cookies and serves to protect from CSRF
-                    sameSite: 'strict',
-                    // only sent over HTTPS
-                    secure: process.env.NODE_ENV === 'production',
-                    // set cookie to expire after a month
-                    maxAge: maxAge * 14
-                });
-            }
-            
-        }
-    }
-    
+				setCookie = cookie.serialize('accessToken', newAccessToken, {
+					// send cookie for every page
+					path: '/',
+					// server side only cookie so you can't use `document.cookie`
+					httpOnly: true,
+					// only requests from same site can send cookies and serves to protect from CSRF
+					sameSite: 'strict',
+					// only sent over HTTPS
+					secure: process.env.NODE_ENV === 'production',
+					// set cookie to expire after a month
+					maxAge: maxAge * 14
+				});
+			}
+		}
+	}
 
-    /* if (!cookies.session){
+	/* if (!cookies.session){
         return await resolve(event);
     }
 
@@ -139,23 +135,22 @@ export const handle: Handle =async ({ event, resolve }) => {
         event.locals = { username: session.username }
     }
  */
-    console.log("==return==");
-    //event.locals.user = { id: 9999, email: "email@email.email" };
-    console.log("==return==>",event.locals);
-    const response = await resolve(event);
-    response.headers.set('Set-Cookie', setCookie);
-    return response;
-    
-}
+	console.log('==return==');
+	//event.locals.user = { id: 9999, email: "email@email.email" };
+	console.log('==return==>', event.locals);
+	const response = await resolve(event);
+	response.headers.set('Set-Cookie', setCookie);
+	return response;
+};
 
 export const getSession: GetSession = ({ locals }) => {
-    console.log("getSession==>",locals);
-    if (!locals.user) return {}
+	console.log('getSession==>', locals);
+	if (!locals.user) return {};
 
-    return {
-        user: {
-            id: locals.user.id,
-            email: locals.user.email,
-        }
-    }
-}
+	return {
+		user: {
+			id: locals.user.id,
+			email: locals.user.email
+		}
+	};
+};
--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -1,26 +1,27 @@
-import "dotenv/config"
-import { sign } from "jsonwebtoken";
+import 'dotenv/config';
+import pkg from 'jsonwebtoken';
+const { sign } = pkg;
 
 export const maxAge = 1 * 24 * 60 * 60;
 export const createToken = (id: number) => {
-    const jwt_secret: string = process.env.JWT_SECRET || '';
-    return sign({id}, jwt_secret, {
-        expiresIn: maxAge
-    });
-}
+	const jwt_secret: string = process.env.JWT_SECRET || '';
+	return sign({ id }, jwt_secret, {
+		expiresIn: maxAge
+	});
+};
 
 export const createRefreshToken = () => {
-    const jwt_secret: string = process.env.JWT_SECRET || '';
-    return sign({}, jwt_secret, {
-        expiresIn: '14d',
-        issuer: 'lj'
-    })
-}
+	const jwt_secret: string = process.env.JWT_SECRET || '';
+	return sign({}, jwt_secret, {
+		expiresIn: '14d',
+		issuer: 'lj'
+	});
+};
 
 export const createAccessToken = (id: number, email: string) => {
-    const jwt_secret: string = process.env.JWT_SECRET || '';
-    return sign({id, email}, jwt_secret, {
-        expiresIn: '1h',
-        issuer: 'lj'
-    });
-}
+	const jwt_secret: string = process.env.JWT_SECRET || '';
+	return sign({ id, email }, jwt_secret, {
+		expiresIn: '1h',
+		issuer: 'lj'
+	});
+};
